22d9501e867d3232dd062ef5b58055f1ff03cf20
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-01-04 21:07:15 +0530

    Learning Binary Search

---
+++ Binary Search/BinarySearch.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int arr[],int size,int key){
+
+    int start = 0;
+    int end = size - 1;
+    // int mid = (start + end)/2;
+    int mid = start + ((end-start)/2);
+
+    while(start <= end){
+
+        if(arr[mid]==key){ // key found condition
+            return mid;
+        }
+
+        if(key > arr[mid]){ //going right
+           
+            start = mid + 1;
+
+        } else { // going left
+            
+            end = mid - 1;
+
+        }
+        
+        mid = start + ((end-start)/2);
+
+    }
+    return -1;
+
+}
+
+int main(){
+
+    int even[6] = {2,4,6,8,10,12};
+    int odd[5] = {3,5,7,9,10};
+
+    int evenArr = BinarySearch(even,6,10);
+    cout<<"Index of 10 : "<<evenArr<<endl;
+
+    int oddArr = BinarySearch(odd,5,10);
+    cout<<"Index of 10 : "<<oddArr<<endl;
+
+    return 0;
+}
