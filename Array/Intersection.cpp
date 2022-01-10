1ac3265f7fbd834791322f26e16e58062e40053e
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-01-10 21:37:39 +0530

    Create Intersection.cpp

---
+++ Array/Intersection.cpp
@@ -0,0 +1,9 @@
+/*
+Problem Statement (https://bit.ly/3Il0c7n)
+You are given two arrays 'A' and 'B' of size 'N' and 'M' respectively. Both these arrays are sorted in non-decreasing order. You have to find the intersection of these two arrays.
+Intersection of two arrays is an array that consists of all the common elements occurring in both arrays.
+*/
+
+#include<iostream>
+using namespace std;
+
