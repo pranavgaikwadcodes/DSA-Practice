3235c3654fd198460eb0ce15f9797f6d9bd85693
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-01-16 21:10:12 +0530

    Create EuclidsAlgorithm.cpp

---
+++ Basic Maths/Euclid’s Algorithm/EuclidsAlgorithm.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b){
+
+    if( a == 0 )
+        return b;
+    
+    if( b == 0 )
+        return a;
+    
+    while(a != b){
+
+        if(a > b){
+            a = a - b;
+        }
+        else{
+            b = b - a;
+        }
+    }
+    
+    return a;
+}
+
+
+int main() {
+
+    int a,b;
+    cout << " Enter val of a and b : " << endl;
+    cin >> a >> b;
+
+    int ans = gcd(a,b);
+
+    cout << endl << " Teh GCD of " << a << " and " << b << " is -> " << ans << endl;
+    
+    return 0;
+}
