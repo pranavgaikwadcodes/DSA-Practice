ea05ec31955a55a13d276a6e7e8ad8783a8e2159
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-01-21 15:19:45 +0530

    Create DynamicArrays.cpp

---
+++ Array/DynamicArrays/DynamicArrays.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int getSum(int *arr, int n) {
+    
+    int sum = 0;
+    for(int i=0; i<n; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+    
+    //variable size array
+    int* arr = new int[n];
+
+    //takign inputn in aray
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    int ans = getSum(arr, n);
+    cout << "answer is "<< ans << endl;
+
+    return 0;
+}
