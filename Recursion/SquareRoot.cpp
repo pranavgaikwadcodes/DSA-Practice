96ecfbc49666923b6b5c824b55b79a40bae6c003
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-01-26 03:09:53 +0530

    Update SquareRoot.cpp

--- Recursion/SquareRoot.cpp
+++ Recursion/SquareRoot.cpp
@@ -1,44 +1,41 @@
-// this solution is not working properly 
+//only works for perfect square
 
+#include<iostream>
+using namespace std;
 
-// #include<iostream>
-// using namespace std;
-
-// long long int squareRoot(int n, int start, int end){
+long long int squareRoot(int n, int start, int end){
     
-//     //base case
-//     if(n == 0 || n == 1)
-//         return n;
+    //base case
+    if(n == 0 || n == 1)
+        return n;
 
-//     if(start >= end){
-//         return end;
-//     }
+    int mid = start + (end - start)/2;
+    long long int sq = mid * mid;
 
-//     int mid = start + (end - start)/2;
-//     long long int sq = mid * mid;
+    if(start > end){
+        return mid;
+    }
 
-//     cout <<mid<<endl;
+    if(sq == n) {
+        return mid;
+    }
+
+    if(sq < n) {
+        return squareRoot(n,mid+1,end);
+    }
+    else {
+        return squareRoot(n,start,mid-1);
+    }
     
-//     if(sq == n) {
-//         return mid;
-//     }
+}
 
-//     if(sq < n) {
-//         return squareRoot(n,mid+1,end);
-//     }
-//     else {
-//         return squareRoot(n,start,mid-1);
-//     }
+int main() {
 
-// }
+    int n;
+    cin >> n;
 
-// int main() {
-
-//     int n;
-//     cin >> n;
-
-//     int start = 0, end = n;
-//     long long int ans = squareRoot(n,start,end);
-//     cout << endl << ans << endl;
-//     return 0;
-// }
+    int start = 0, end = n;
+    long long int ans = squareRoot(n,start,end);
+    cout << endl << ans << endl;
+    return 0;
+}
