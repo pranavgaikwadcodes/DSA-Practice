080f4627b4b17e33af16b41437e53dad8b95b73d
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-03-14 18:05:43 +0530

    Update CLL.cpp

--- LinkedList/CLL.cpp
+++ LinkedList/CLL.cpp
@@ -22,6 +22,52 @@ class Node{
     }
 };
 
+void insertNode( Node* &tail , int position , int data ) {
+
+    // Linked List is empty
+    if( tail == NULL ){
+        Node* temp = new Node(data);
+        tail = temp;
+        temp -> next = temp;
+    }
+    else { // List is not empty
+        Node* current = tail;
+
+        while(current-> data != position){
+            current = current -> next;
+        }
+
+        Node* temp = new Node(data);
+        temp -> next = current -> next;
+        current -> next = temp;
+
+    }
+
+}
+
+void print( Node* tail ){
+    Node* temp = tail;
+
+    do {
+        cout << tail -> data << " ";
+        tail = tail -> next;
+    }
+    while( tail != temp );
+    
+    cout << endl;
+}
+
 int main() {
+
+    Node* tail = NULL;
+
+    insertNode(tail,1,3);    
+    insertNode(tail,3,5);
+    insertNode(tail,3,4);
+    insertNode(tail,5,6);
+
+    print(tail);
+
+
     return 0;
 }
