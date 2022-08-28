35903f6bb3f3938ad24be3802024c48588a51e5d
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2022-08-28 17:21:22 +0530

    Create stackUsingArray.cpp

---
+++ Stack/stackUsingArray.cpp
@@ -0,0 +1,82 @@
+// stack implementation using array
+
+#include<iostream>
+using namespace std;
+
+class Stack{
+    // properties
+    public:
+        int *arr;
+        int top;
+        int size;
+
+
+    // behaviour
+    Stack(int size){
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element){
+        if(size - top > 1){
+            top++;
+            arr[top] = element;
+        }else{
+            cout<< " Stack overflow. "<< endl;
+        }
+    }
+
+    void pop(){
+        if(top >= 0){
+            top--;
+        }
+        else{
+            cout << "stack underflow" << endl;
+        }
+    }
+
+    int peek(){
+        if(top >= 0){
+            return arr[top];
+        }
+        else{
+            cout << "stack is empty" << endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+};
+
+int main() {
+
+    Stack s(5);
+    s.push(11);
+    s.push(16);
+    s.push(10);
+
+    cout << s.peek() << endl;
+    
+    s.pop();
+    // s.pop();
+    // s.pop();
+    // s.pop();
+
+    cout << s.peek() << endl;
+
+    if(s.isEmpty())
+        cout << "Stack is empty." << endl;
+    else
+        cout << "Stack is not empty." << endl;
+
+    return 0;
+}
